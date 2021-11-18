2e39a115ddadccaa16f802f14206dec431554b9c
Whiskey <825906196@qq.com>
2021-11-19 00:43:21 +0800

    fix

---
+++ .history/src/components/Info/Introduce/index_20211119000033.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="introduce">
+      <el-row class="intContainer">
+          <el-col :span="6">
+            <transition name="fade">
+                <div id="title" class="fadeIn">{{ name }}</div>
+            </transition>
+            <div id="subTitle">{{ enName }}</div>
+          </el-col>
+          <el-col :span="10">
+              <el-image class="logo" alt="logo" :src="require('@/icons/voice.png')"></el-image>
+          </el-col>
+      </el-row>
+  </div>
+</template>
+
+<script>
+const animalsData = require('@/data/Animals.json')
+import store from '../../../store'
+export default {
+    data() {
+        return {
+            name: '鸡',
+            enName: 'Chicken'
+        }
+    },
+    // mounted() {
+    //     this.changeInfo()
+    //     // window.console.log(store.state.num)
+    // },
+    // methods: {
+    //       changeInfo() {
+    //         setInterval(function() {
+    //             this.name = animalsData.AnimalsDetail[store.state.num].name
+    //             document.getElementById('title').innerHTML = this.name
+    //             // window.console.log(this.name)
+    //             this.enName = animalsData.AnimalsDetail[store.state.num].enName
+    //             document.getElementById('subTitle').innerHTML = this.enName
+    //             // window.console.log(this.enName)
+    //         }, store.state.seconds);
+    //     }
+    // },
+    computed: {
+        nameChange() {
+            return store.state.num
+        }
+    },
+    watch: {
+        nameChange: function() {
+             this.name = animalsData.AnimalsDetail[store.state.num].name
+             document.getElementById('title').innerHTML = this.name
+             this.enName = animalsData.AnimalsDetail[store.state.num].enName
+             document.getElementById('subTitle').innerHTML = this.enName
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../styles/transition.scss';
+
+@keyframes fadeOutIn
+{
+    0%   {opacity: 1;}
+    50%  {opacity: 0.2;}
+    100%  {opacity: 1;}
+}
+.fadeIn {
+    // animation: fadeIn 5s ease;
+}
+.introduce {
+    // background-color: wheat;;
+    height: 30%;
+    width: 100%;
+    animation: fadeOutIn 2s ease;
+}
+.intContainer {
+    padding: 15px 0;
+}
+#title {
+    // font-size: 60px;
+    font-size: 3vw;
+    display: block;
+}
+#subTitle {
+    // font-size: 44px;
+    font-size: 2.3vw;
+    display: block;
+}
+.logo {
+    padding-top: 3.5vw;
+    height: 3.1vw;
+    width: 3.1vw;
+}
+
+</style>
---
+++ .history/src/views/Loading_20211118221404.vue
@@ -0,0 +1,38 @@
+<template>
+   <svg viewBox="0 0 800 600" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+        <line class="mark" x1="0" y1="0" x2="0" y2="0" fill="none" stroke="#5BB4D8" stroke-miterlimit="10" stroke-linecap="none" stroke-width="12"/>
+        </defs>
+        <g class="container1"/>
+        <g class="container2"/>
+   </svg>
+  <script src='js/TweenMax.min.js'></script>
+
+
+    <script  src="js/index.js"></script>
+
+
+<div style="text-align:center;clear:both">
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+body {
+ background-color: #FFFCF9;
+ overflow: hidden;
+ text-align:center;
+
+}
+svg {
+ width: 100%;
+ height: 100%;
+ visibility: hidden;
+
+}
+</style>>
