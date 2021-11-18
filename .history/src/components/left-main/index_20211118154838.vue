c86b48c9ea91c505c3087d9e9326baae30980986
Whiskey <825906196@qq.com>
2021-11-18 16:08:14 +0800

    img

---
+++ .history/src/components/left-main/index_20211118154838.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="leftContainer">
+    <el-row :gutter="20" type="flex" justify="center" v-for="(i, inx) in dataLength/3" :key="inx">
+      <el-col :span="8" v-for="(item, index) in getNum(inx * 3)" :key="index">
+        <el-image class="logo" alt="logo" :src="require('@/icons/animals/chicken.png')"></el-image>
+        <div>{{ item.enName }}</div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { getData } from '@/api/data'
+const animalsData = require('@/data/Animals.json')
+export default {
+  data() {
+    return {
+      data: {},
+      dataLength: ''
+    }
+  },
+  created() {
+    this.getDatabase()
+  },
+  mounted() {
+    this.data = animalsData
+    this.dataLength = this.data.AnimalsDetail.length
+  },
+  methods: {
+      getNum(inx){
+        // window.console.log(this.data.AnimalsDetail.slice(inx, inx + 3))
+        return this.data.AnimalsDetail.slice(inx, inx + 3)
+      },
+      getDatabase() {
+        var params = {}
+        getData(params).then(res => {
+          window.console.log(res)
+        })
+      }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.leftContainer {
+  padding: 30px 0;
+  width: 100%;
+  text-align: center;
+  color: #e6e6e6;
+  font-size: 1.3vw;
+}
+.leftContainer .logo {
+  width: 80px;
+  height: 80px;
+  padding: 40px 0;
+}
+ @media screen and (max-width: 1440px) { // 屏幕小于1440px样式
+  .leftContainer .logo {
+    height: 50px;
+    width: 50px;
+    padding: 30px 0;
+  }
+ }
+</style>
